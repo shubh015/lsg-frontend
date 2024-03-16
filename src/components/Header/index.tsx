096c8ce5351d956d550591f3902cbd2d6bddd486
Deepak Verma <deepak@webllisto.com>
2024-03-16 16:26:08 +0530

    fix material ui version issue

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,34 +16,30 @@ const Header: React.FC = () => {
         <div className="inner_header_wrp flex gap-4 items-center">
           <div className="flex gap-4 items-center">
             <BsCalendarDate color="red" />
-            <Typography placeholder={''} className="font-sans font-bold">
-              Date
-            </Typography>
+            <Typography className="font-sans font-bold">Date</Typography>
             <p>|</p>
           </div>
           <div className="flex gap-4">
-            <Typography placeholder={''} className="font-sans font-bold">
+            <Typography className="font-sans font-bold">
               Privacy Policy
             </Typography>
             <p>|</p>
           </div>
           <div className="flex gap-4">
-            <Typography placeholder={''} className="font-sans font-bold">
+            <Typography className="font-sans font-bold">
               Terms & Conditions
             </Typography>
             <p>|</p>
           </div>
           <div className="flex items-center gap-2">
-            <Typography placeholder={''} className="font-sans font-bold">
-              Light Mode
-            </Typography>
+            <Typography className="font-sans font-bold">Light Mode</Typography>
             <CiLight />
           </div>
         </div>
         <div className="login_wrp flex items-center gap-4">
           <div className="flex items-center gap-2">
             <CiUser color="red" />
-            <Typography placeholder={''} className="font-sans font-bold">
+            <Typography className="font-sans font-bold">
               Login/Register
             </Typography>
           </div>
