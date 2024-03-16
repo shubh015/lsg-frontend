e25adf52144b0273e9c2bd06606d6435c82a3400
Deepak Verma <deepak@webllisto.com>
2024-03-16 16:50:44 +0530

    fix ui issue

--- src/components/Header/Marquee.tsx
+++ src/components/Header/Marquee.tsx
@@ -7,8 +7,8 @@ const Marquee: React.FC = () => {
   return (
     <div className="container">
       <div className="marquee_wrp">
-        <div className=" flex items-start">
-          <div className="bg-red-400">
+        <div className="flex justify-center">
+          <div className="bg-red-400 flex justify-center items-center px-2">
             <Typography
               className="font-sans font-bold text-center text-white"
               variant="h6"
@@ -17,7 +17,7 @@ const Marquee: React.FC = () => {
               Trending Articles
             </Typography>
           </div>
-          <div className="flex overflow-x-hidden overflow-y-hidden bg-gray-400 justify-center w-full py-[13px] ">
+          <div className="flex overflow-x-hidden overflow-y-hidden bg-gray-400 justify-center w-full py-[13px] flex-1">
             <div className="top-0 animate-marquee flex whitespace-nowrap">
               <Typography
                 className="font-sans mx-4 flex items-center gap-2 font-semibold"
